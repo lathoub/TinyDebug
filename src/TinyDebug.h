63822bad1a73b52fe79a2384c04c97317dd3b011
lathoub <4082369+lathoub@users.noreply.github.com>
2021-04-18 08:52:11 +0200

    Update TinyDebug.h

--- src/TinyDebug.h
+++ src/TinyDebug.h
@@ -1,47 +1,64 @@
-
-#ifdef SerialMon
+/*#ifdef SerialMon
 #define DEBUG_PRINT(...) SerialMon.print(__VA_ARGS__)
 #define DEBUG_PRINTLN(...) SerialMon.println(__VA_ARGS__)
 #else
 #define DEBUG_PRINT(...)
 #define DEBUG_PRINTLN(...)
 #endif
-/*
+*/
+#ifdef SerialMon
+
+#if defined(__AVR__) && !defined(__AVR_ATmega4809__)
+#define TINY_GSM_PROGMEM PROGMEM
+typedef const __FlashStringHelper *GsmConstStr;
+#define GFP(x) (reinterpret_cast<GsmConstStr>(x))
+#define GF(x) F(x)
+#else
+#define TINY_GSM_PROGMEM
+typedef const char *GsmConstStr;
+#define GFP(x) x
+#define GF(x) x
+#endif
+
+namespace
+{
+  template <typename T>
+  static void DBG_PLAIN(T last)
+  {
+    SerialMon.println(last);
+  }
+
+  template <typename T, typename... Args>
+  static void DBG_PLAIN(T head, Args... tail)
+  {
+    SerialMon.print(head);
+    SerialMon.print(' ');
+    DBG_PLAIN(tail...);
+  }
+
+  template <typename... Args>
+  static void DBG(Args... args)
+  {
+    static unsigned long previous = 0;
+    SerialMon.print(GF("["));
+    SerialMon.print(millis());
+    SerialMon.print(GF("] "));
+    DBG_PLAIN(args...);
+  }
+} // namespace
+
 void debug_setup()
 {
   // Set console baud rate
-#ifdef SerialMon
   SerialMon.begin(115200);
-  while (!SerialMon) {}
-  SerialMon.println(F("\nBooting..."));
-#endif
-}
-*/
-/*
-#ifdef SerialMon
-namespace {
-template <typename T>
-static void DBG_PLAIN(T last) {
-  SerialMon.println(last);
+  while (!SerialMon)
+  {
+  }
+  DBG(F("\nBooting..."));
 }
 
-template <typename T, typename... Args>
-static void DBG_PLAIN(T head, Args... tail) {
-  SerialMon.print(head);
-  SerialMon.print(' ');
-  DBG_PLAIN(tail...);
-}
-
-template <typename... Args>
-static void DBG(Args... args) {
-  SerialMon.print(GF("["));
-  SerialMon.print(millis());
-  SerialMon.print(GF("] "));
-  DBG_PLAIN(args...);
-}
-}  // namespace
 #else
 #define DBG_PLAIN(...)
 #define DBG(...)
+void debug_setup() {}
 #endif
-*/
